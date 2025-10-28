b30abe2073e10371aae34d80bb3ea8edeba08118
Maximiliano Duthey <maxi.duthey@txpipe.io>
2025-10-28 12:55:11 -0300

    feat: migrate from tanstack-router only to tanstack start

--- frontends/dashboard/vite.config.ts
+++ frontends/dashboard/vite.config.ts
@@ -1,24 +1,32 @@
-import { resolve } from 'node:path';
-
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { devtools } from '@tanstack/devtools-vite';
 
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, './src'),
-    },
+  server: {
+    port: 3000,
   },
+  plugins: [
+    devtoolsJson(),
+    devtools({
+      removeDevtoolsOnBuild: true,
+    }),
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    nitroV2Plugin(/*
+      // nitro config goes here, e.g.
+      { preset: 'node-server' }
+    */),
+    viteReact(),
+  ],
 });
