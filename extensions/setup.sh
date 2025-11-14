4527e63831c4fdbf592fc8265023d3d0fc6f2ecc
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-11-14 11:06:58 -0300

    chore: improve setup script

--- extensions/setup.sh
+++ extensions/setup.sh
@@ -3,18 +3,19 @@ kind create cluster --name supernode --config config.yml
 kubectl config use-context kind-supernode 
 
 # Install control plane
-helm install control-plane oci://supernode.store/control-plane \
-  --version 0.2.0-rc1 \
+helm install control-plane ./control-plane/ \
   --namespace control-plane \
   --create-namespace
 
-# # Dolos
-# helm install dolos ./dolos \
-#   --namespace dolos \
-#   --create-namespace \
-#   --values midnight-kind-values.yaml \
-#   --set image.tag=v0.32.0 \
-#   --set config.upstreamAddress=relay.cnode-m1.demeter.run:3002
+# Dolos
+helm install dolos-preview ./dolos \
+  --namespace dolos \
+  --create-namespace \
+  --set image.tag=v0.32.0 \
+  --set extraLabels.supernode/status=ready \
+  --set config.upstreamAddress=relay.cnode-m1.demeter.run:3002 \
+  --set node.network=preview \
+  --set node.networkMagic=2
 
 # # Hydra
 # kubectl create namespace hydra
@@ -36,42 +37,34 @@ helm install control-plane oci://supernode.store/control-plane \
 # #  --set keys.hydraVerification.items[0].filename=hydra.vk
 
 # # Cardano node
-# helm install cardano-node ./cardano-node/ \
-#   --namespace cardano-node \
-#   --create-namespace
+helm install cardano-preview ./cardano-node/ \
+  --namespace cardano-node \
+  --set extraLabels.supernode/status=ready \
+  --create-namespace \
+  --set node.network=preview \
+  --set node.networkMagic=2
 
 # Midnight
 kubectl create namespace midnight
 openssl rand -hex 32 > /tmp/midnight-node.privatekey
 kubectl -n midnight create secret generic midnight-node-key \
  --from-file=node.key=/tmp/midnight-node.privatekey
-# helm install midnight oci://supernode.store/extensions/midnight \
-#   --version 0.2.0-rc3 \
+helm install midnight ./midnight/ \
+  --namespace midnight \
+  --values midnight-kind-values.yaml \
+  --set extraLabels.supernode/status=ready \
+  --set nodeKey.existingSecret.name=midnight-node-key \
+  --set nodeKey.existingSecret.key=node.key
+
+# helm upgrade midnight ./midnight/ \
 #   --namespace midnight \
 #   --values midnight-kind-values.yaml \
 #   --set nodeKey.existingSecret.name=midnight-node-key \
 #   --set nodeKey.existingSecret.key=node.key
-helm install midnight ./midnight/ \
-  --namespace midnight \
-  --values midnight-kind-values.yaml \
-  --set nodeKey.existingSecret.name=midnight-node-key \
-  --set nodeKey.existingSecret.key=node.key
 
 # # Apex fusion
-# helm install vector-tesnet ./apex-fusion \
-#   --namespace apex-fusion \
-#   --create-namespace \
-#   --set node.network=vector-testnet \
-#   --set node.networkMagic=1
-#
-# helm install prime-testnet ./apex-fusion \
-#   --namespace apex-fusion \
-#   --create-namespace \
-#   --set node.network=prime-testnet \
-#   --set node.networkMagic=1
-#
-# helm install prime-mainnet ./apex-fusion \
-#   --namespace apex-fusion \
-#   --create-namespace \
-#   --set node.network=prime-mainnet \
-#   --set node.networkMagic=1
+helm install prime-testnet ./apex-fusion \
+  --namespace apex-fusion \
+  --create-namespace \
+  --set extraLabels.supernode/status=ready \
+  --set node.network=prime-testnet
