872bfb54be23fc30c9e0f08a869a35666324cb54
Maximiliano Duthey <maxi.duthey@txpipe.io>
2025-11-14 22:35:59 -0300

    feat: add new deployment section

--- frontends/catalog/src/routes/index.tsx
+++ frontends/catalog/src/routes/index.tsx
@@ -7,6 +7,7 @@ import { Container } from '~/components/ui/Container';
 import { HeroSection } from '~/sections/home/Hero';
 import { RegisterBetaSection } from '~/sections/home/RegisterBeta';
 import { CuratedCatalogSection } from '~/sections/home/CuratedCatalog';
+import { DeploymentSection } from '~/sections/home/Deployment';
 
 export const Route = createFileRoute('/')({
   component: LandingPage,
@@ -17,6 +18,7 @@ function LandingPage() {
     <Container className="px-0 sm:px-0 py-0">
       <HeroSection />
       <CuratedCatalogSection />
+      <DeploymentSection />
       <RegisterBetaSection />
     </Container>
   );
