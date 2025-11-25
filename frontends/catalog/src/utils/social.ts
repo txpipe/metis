f073d980849585b24af210f76227426cea5a17f9
Maximiliano Duthey <maxi.duthey@txpipe.io>
2025-11-25 14:03:35 -0300

    chore: missing type on social metadata

--- frontends/catalog/src/utils/social.ts
+++ frontends/catalog/src/utils/social.ts
@@ -3,10 +3,12 @@ export function generateSocialMetadata(title: string, description: string, image
     return [];
   }
   return [
+    { property: 'og:type', content: 'website' },
     { property: 'og:title', content: title },
     { property: 'og:description', content: description },
     { property: 'og:image', content: imageUrl },
     { property: 'og:url', content: pageUrl },
+    { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: title },
     { name: 'twitter:description', content: description },
     { name: 'twitter:image', content: imageUrl },
